cd62ed22acc680d1231531f8c894c23742567d21
すー <s1732087ap@s.chibakoudai.jp>
2018-09-04 01:32:42 +0900

    a

--- Program.cs
+++ Program.cs
@@ -12,6 +12,39 @@ namespace CsharpRanking
     {
         static void Main(string[] args)
         {
+            var www = new WWWRunking();
+            www.GetData();
+        }
+    }
+
+    class WWWRunking
+    {
+        public void GetData()
+        {
+            string url = "http://localhost/runking/GetData.php";
+
+            //文字コードを指定する
+            System.Text.Encoding enc =
+                System.Text.Encoding.GetEncoding("UTF-8");
+
+            //POST送信する文字列を作成
+            string postData =
+                "id=1&word=" +
+                System.Web.HttpUtility.UrlEncode("keyword", enc);
+
+            System.Net.WebClient wc = new System.Net.WebClient();
+            //文字コードを指定する
+            wc.Encoding = enc;
+            //ヘッダにContent-Typeを加える
+            wc.Headers.Add("Content-Type", "application/plain");
+            //データを送信し、また受信する
+            string resText = wc.UploadString(url, postData);
+            Console.WriteLine(postData);
+            wc.Dispose();
+
+            //受信したデータを表示する
+            Console.WriteLine("data：");
+            Console.WriteLine(resText);
         }
     }
 }
