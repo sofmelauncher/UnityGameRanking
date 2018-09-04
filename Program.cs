5dcea85cbdbe14dc05e6c3dc7f22eccd702c8aae
すー <s1732087ap@s.chibakoudai.jp>
2018-09-04 18:11:40 +0900

    サーバー設定、ドキュメンテーションコメント追加

--- Program.cs
+++ Program.cs
@@ -12,39 +12,112 @@ namespace CsharpRanking
     {
         static void Main(string[] args)
         {
-            var www = new WWWRunking();
-            www.GetData();
+            var w = new WWWRunking();
+            w.PostString = "aaa";
+            if (w.init())
+            {
+                w.GetData();
+            }
+            else
+            {
+                Console.WriteLine("filed init.");
+            }
         }
     }
-
+    ///<summary>
+    ///外部サーバーへのランキング機能を提供
+    ///接続失敗時ローカルのデータベースを利用する
+    ///</summary>
     class WWWRunking
     {
+        //= "http://localhost/runking/GetData.php";
+
+        public string BaseUrl { private set; get; }
+        public string PostString { get; set; }
+        public string ResData { private set; get; }
+
+        private string PostData { get; set; }
+        private Encoding enc = Encoding.GetEncoding("UTF-8");
+        private string ConfigFilePath = ConfigPath.LocalUserAppDataPath + "/config.txt";
+
+        private const string GetDataUrl = "/runking/GetData.php";
+
+        public WWWRunking()
+        {
+        }
+
+        ///<summary>
+        ///外部データベースに接続
+        ///</summary>
+        ///<returns>true:接続成功, false:接続失敗</returns>
+        public bool init()
+        {
+            if (this.LoadServerAdress())
+            {
+                Console.WriteLine("Conection to server.");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        ///<summary>
+        ///外部データベースからデータ取得
+        ///</summary>
         public void GetData()
         {
-            string url = "http://localhost/runking/GetData.php";
-
-            //文字コードを指定する
-            System.Text.Encoding enc =
-                System.Text.Encoding.GetEncoding("UTF-8");
-
-            //POST送信する文字列を作成
-            string postData =
-                "id=1&word=" +
-                System.Web.HttpUtility.UrlEncode("keyword", enc);
+            this.PostData = "id=1&word=" + System.Web.HttpUtility.UrlEncode(PostString, enc);
 
             System.Net.WebClient wc = new System.Net.WebClient();
-            //文字コードを指定する
             wc.Encoding = enc;
-            //ヘッダにContent-Typeを加える
+
             wc.Headers.Add("Content-Type", "application/plain");
-            //データを送信し、また受信する
-            string resText = wc.UploadString(url, postData);
-            Console.WriteLine(postData);
+            try
+            {
+                ResData = wc.UploadString(BaseUrl + GetDataUrl, PostData);
+            }
+            catch(System.Net.WebException ex)
+            {
+                Console.WriteLine("Connection to server failed.");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(PostData);
             wc.Dispose();
 
-            //受信したデータを表示する
             Console.WriteLine("data：");
-            Console.WriteLine(resText);
+            Console.WriteLine(ResData);
+        }
+        ///<summary>
+        ///ローカルにあるサーバーアドレス情報読み込み
+        ///</summary>
+        ///<returns>true:読み込み成功, false:読み込み失敗</returns>
+        private bool LoadServerAdress()
+        {
+            System.IO.StreamReader sr;
+
+            try
+            {
+                sr = System.IO.File.OpenText(ConfigFilePath);
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                System.Console.WriteLine("File not fount.");
+                System.Console.WriteLine(ex.Message);
+                return false;
+            }
+            catch(System.UnauthorizedAccessException ex)
+            {
+                System.Console.WriteLine("Access denied.");
+                System.Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            this.BaseUrl = sr.ReadToEnd();
+            sr.Close();
+            return true;
         }
     }
 }
