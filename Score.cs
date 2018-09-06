e66a375a021a99ac31993fdd5c94d34e49666773
すー <s1732087ap@s.chibakoudai.jp>
2018-09-07 00:37:32 +0900

    add score

---
+++ Score.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpRanking
+{
+    class Score
+    {
+        
+        public static ScoreType Type { private set; get; }
+
+        public string ScoreValue { private set; get; }
+
+        public static void SetScoreType(ScoreType type)
+        {
+            Type = type;
+            return;
+        }
+
+        public double ToDouble()
+        {
+            return double.Parse(this.ScoreValue);
+        }
+
+        public TimeSpan ToTime()
+        {
+            long t = long.Parse(ScoreValue);
+            TimeSpan ts = new TimeSpan(t);
+            return ts;
+        }
+    }
+}
