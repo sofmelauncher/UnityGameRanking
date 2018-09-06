6f6a194108e208e5b5324dd46e9445f69c322ef5
すー <s1732087ap@s.chibakoudai.jp>
2018-09-07 00:37:46 +0900

    add RankingData

---
+++ RankingData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpRanking
+{
+    class RankingData
+    {
+        public static UInt64 GameID { private set; get; }
+        public UInt64 DataID { private set; get; }
+        public DateTime SaveTime { private set; get; }
+
+        public string DataName { private set; get; }
+        public Score Score { private set; get; }
+
+        public static bool SetGameID(UInt64 id)
+        {
+            if (id >= 100) return false;
+            GameID = id;
+            return true;
+        }
+
+
+
+    }
+}
