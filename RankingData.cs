8171c1637c910816784d45e4e292dcfc599ed843
すー <aaaiiiuuu35@gmail.com>
2018-09-07 17:01:48 +0900

    スコアデータ追加, postデータ作成メソッド

--- RankingData.cs
+++ RankingData.cs
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 
 namespace CsharpRanking
 {
-    class RankingData
+    public class RankingData
     {
         public static UInt64 GameID { private set; get; }
+
         public UInt64 DataID { private set; get; }
         public DateTime SaveTime { private set; get; }
 
         public string DataName { private set; get; }
-        public Score Score { private set; get; }
+        //private Score Score { set; get; }
+
+        public static void SetScoreType(ScoreType type)
+        {
+            Type = type;
+            return;
+        }
+        public static ScoreType Type { private set; get; }
+        public string ScoreValue { private set; get; }
+
+        public RankingData(string data, string name = "")
+        {
+            this.ScoreValue = data;
+            this.DataName = name;
+        }
 
         public static bool SetGameID(UInt64 id)
         {
@@ -22,7 +37,28 @@ namespace CsharpRanking
             return true;
         }
 
+        public double ToDouble()
+        {
+            return double.Parse(this.ScoreValue);
+        }
 
+        public TimeSpan ToTime()
+        {
+            long t = long.Parse(ScoreValue);
+            TimeSpan ts = new TimeSpan(t);
+            return ts;
+        }
+
+        public Dictionary<string, string> Dictionary()
+        {
+            return new Dictionary<string, string>
+                {
+                    { "GameID", RankingData.GameID.ToString() },
+                    { "DataID", "0" },
+                    { "DataName", this.DataName },
+                    { "Score", this.ScoreValue }
+                };
+        }
 
     }
 }
