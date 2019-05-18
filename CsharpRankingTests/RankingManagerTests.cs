770357d3f17738faead23ded7f9dcb87b762dd0b
すー <aaaiiiuuu35@gmail.com>
2019-05-18 10:26:47 +0900

    add test class

---
+++ CsharpRankingTests/RankingManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ranking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranking.Tests
+{
+    [TestClass()]
+    public class RankingManagerTests
+    {
+        [TestMethod()]
+        public void RankingManagerTest()
+        {
+            RankingManager.Inst.Setting("神のゲーム", 1, Ranking.OrderType.DESC, false);
+
+            RankingManager.Inst.SaveData(10.5, "ジェイソン");
+
+            //データ取得
+            //例
+            var data = RankingManager.Inst.GetData();
+            foreach (var e in data)
+            {
+                Console.WriteLine(e.ScoreValue);
+                Console.WriteLine(e.DataName);
+            }
+        }
+    }
+}
